25ef3c1452a7a75cb520bf5a0f054c45bf7f75de
Dustin S <texnofobix@users.noreply.github.com>
2015-03-01 19:03:56 -0500

    Create bottleq2.py

---
+++ bottleq2.py
@@ -0,0 +1,109 @@
+"""
+Copyright 2015 - texnofobix
+"""
+
+
+import ConfigParser
+config = ConfigParser.RawConfigParser()
+configFilePath = r'q2webapi.conf'
+config.read(configFilePath)
+
+
+# set configuration here
+webhost = str(config.get('web', 'host'))
+webport = int(config.get('web', 'port'))
+q2host = str(config.get('q2', 'host'))
+q2port = int(config.get('q2', 'port'))
+q2password = str(config.get('q2', 'password'))
+
+import pyrcon
+import json
+import bottle
+
+app = bottle.Bottle()
+
+@app.hook('after_request')
+def enable_cors():
+    bottle.response.headers['Access-Control-Allow-Origin'] = '*'
+
+@app.route('/')
+def index():
+        return '''
+    <h1>q2webapi</h1>
+    <pre>
+    <a href="/v1/players">/v1/players</a> - Gets all the players on the server.
+    /v1/players/<num> - Gets a specific player info
+
+    <a href="/v1/maps">/v1/maps</a> - Gets all the maps on the server
+    <a href="/v1/maps/current">/v1/maps/current</a> - Gets the current map
+    /v1/maps/<mapname> [get to test if valid / post to change with rcon_password in header]
+    </pre>
+    '''
+
+@app.get('/v1/players')
+def players():
+    conn.status()
+    bottle.response.content_type = 'application/json'
+
+    players = {"players": conn.Players}
+    return json.dumps(players, separators=(',', ': '))
+
+@app.get('/v1/players/<num:int>')
+def player(num):
+    bottle.response.content_type = 'application/json'
+    conn.status()
+    if num <= len(conn.Players) - 1:
+        return json.dumps({"players": conn.Players[num]})
+    return json.dumps({"players": None})
+
+@app.delete('/v1/players/<num:int>')
+def kickPlayer(num):
+    bottle.response.content_type = 'application/json'
+    if bottle.request.headers.get('rcon_password') == q2password:
+        return '{"kickPlayerNum": "' + str(num) + \
+            '","message": "' + str(conn.send("kick " + str(num))) + '"}'
+    return bottle.abort(401, "Sorry, access denied. Missing required rcon_password header with valid password.")
+
+@app.get('/v1/maps')
+def allmaps():
+    bottle.response.content_type = 'application/json'
+    return json.dumps({"maps": conn.maplist()})
+
+@app.get('/v1/maps/current')
+def map_current():
+    conn.status()
+    bottle.response.content_type = 'application/json'
+    return json.dumps({"maps": conn.current_map})
+
+@app.get('/v1/maps/<mapname:re:[a-z0-9]+>')
+def testmap(mapname):
+    if mapname in conn.maplist():
+        setreturn = True
+    else:
+        setreturn = False
+    bottle.response.content_type = 'application/json'
+
+    return '{"mapname": "' + str(mapname) + \
+        '", "mapExists": "' + str(setreturn).lower() + '"}'
+
+
+@app.post('/v1/maps/<mapname:re:[a-z0-9]+>')
+def changemap(mapname):
+    bottle.response.content_type = 'application/json'
+    if bottle.request.headers.get('rcon_password') == q2password:
+        return '{"requestedMap": "' + str(mapname) + \
+            '","changedMap": ' + str(conn.changemap(mapname)).lower() + '}'
+    return bottle.abort(401, "Sorry, access denied. Missing required rcon_password header with valid password.")
+
+@app.post('/v1/rawcommand')
+def rawCommand():
+    bottle.response.content_type = 'application/json'
+    if bottle.request.headers.get('rcon_password') == q2password:
+        command = str(bottle.request.forms.get('command')).strip()
+        print command
+        return '{"command": "' + command + '", "status": "' + conn.send(command) + '"}'
+    return bottle.abort(401, "Sorry, access denied. Missing required rcon_password header with valid password.")
+
+
+conn = pyrcon.q2RConnection(host=q2host, port=q2port, password=q2password)
+app.run(host=webhost, port=webport, reloader=True, debug=True)
