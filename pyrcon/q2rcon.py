bcd48f5e648bac8f9ef731a2df96f8244e9017da
Dustin S <texnofobix@users.noreply.github.com>
2020-04-03 08:59:42 -0400

    Update q2rcon.py
    Issue currently with the servervariables method disabling until fixed.

--- pyrcon/q2rcon.py
+++ pyrcon/q2rcon.py
@@ -80,7 +80,8 @@ class q2RConnection(RConnection):
             return 'no'
 
     def servervariables(self):
-        self.hostname = self.send('hostname').split('" is "')[1][:-2]
-        self.version = self.send('version').split('" is "')[1][:-2]
-        self.sv_gravity = self.send('sv_gravity').split('" is "')[1][:-2]
+        #self.hostname = self.send('hostname').split('" is "')[1][:-2]
+        #self.version = self.send('version').split('" is "')[1][:-2]
+        #self.sv_gravity = self.send('sv_gravity').split('" is "')[1][:-2]
+        pass
 
